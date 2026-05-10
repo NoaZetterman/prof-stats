7ecde48eecf8b02ac225f804d81c7547b47b932d
Noa Zetterman <62833391+NoaZetterman@users.noreply.github.com>
2026-05-10 18:29:49 +0200

    Update crafting to read updated design

--- src/client/java/com/profstats/Profession.java
+++ src/client/java/com/profstats/Profession.java
@@ -8,28 +8,30 @@ enum ProfessionType {
 }
 
 public enum Profession {
-    WOODCUTTING("Woodcutting", "Chopping", 'Ⓒ', ProfessionType.GATHERING),
-    MINING("Mining", "Mining", 'Ⓑ', ProfessionType.GATHERING),
-    FARMING("Farming", "Farming", 'Ⓙ', ProfessionType.GATHERING),
-    FISHING("Fishing", "Fishing", 'Ⓚ', ProfessionType.GATHERING),
+    WOODCUTTING("Woodcutting", "Chopping", null ,'Ⓒ', ProfessionType.GATHERING),
+    MINING("Mining", "Mining",null , 'Ⓑ', ProfessionType.GATHERING),
+    FARMING("Farming", "Farming",null , 'Ⓙ', ProfessionType.GATHERING),
+    FISHING("Fishing", "Fishing",null , 'Ⓚ', ProfessionType.GATHERING),
 
-    ARMOURING("Armouring", "Armouring", 'Ⓗ', ProfessionType.CRAFTING),
-    WEAPONSMITHING("Weaponsmithing", "Weaponsmithing", 'Ⓖ', ProfessionType.CRAFTING),
-    TAILORING("Tailoring", "Tailoring", 'Ⓕ', ProfessionType.CRAFTING),
-    WOODWORKING("Woodworking", "Woodworking", 'Ⓘ', ProfessionType.CRAFTING),
-    JEWELING("Jeweling", "Jeweling", 'Ⓓ', ProfessionType.CRAFTING),
-    COOKING("Cooking", "Cooking", 'Ⓐ', ProfessionType.CRAFTING),
-    SCRIBLING("Scribing", "Scribing", 'Ⓔ', ProfessionType.CRAFTING),
-    ALCHEMISM("Alchemism", "Alchemism", 'Ⓛ', ProfessionType.CRAFTING);
+    ARMOURING("Armouring", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF042", 'Ⓗ', ProfessionType.CRAFTING),
+    WEAPONSMITHING("Weaponsmithing", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF047", 'Ⓖ', ProfessionType.CRAFTING),
+    TAILORING("Tailoring", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF046", 'Ⓕ', ProfessionType.CRAFTING),
+    WOODWORKING("Woodworking", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF048", 'Ⓘ', ProfessionType.CRAFTING),
+    JEWELING("Jeweling", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF044", 'Ⓓ', ProfessionType.CRAFTING),
+    COOKING("Cooking", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF043", 'Ⓐ', ProfessionType.CRAFTING),
+    SCRIBLING("Scribing", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF045", 'Ⓔ', ProfessionType.CRAFTING),
+    ALCHEMISM("Alchemism", null, "\uDAFF\uDFF8\uE053\uDAFF\uDF80\uF041", 'Ⓛ', ProfessionType.CRAFTING);
 
     public final String displayName;
     public final String actionName;
+    public final String screenName;
     public final char symbol;
     public final ProfessionType type;
 
-    Profession(String displayName, String actionName, char symbol, ProfessionType type) {
+    Profession(String displayName, String actionName, String screenName, char symbol, ProfessionType type) {
         this.displayName = displayName;
         this.actionName = actionName;
+        this.screenName = screenName;
         this.symbol = symbol;
         this.type = type;
     }
@@ -49,6 +51,16 @@ public enum Profession {
     public static Profession fromDisplayName(String s) {
         for(Profession p : Profession.values()) {
             if(p.displayName.equals(s)) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public static Profession fromScreenName(String s) {
+        for(Profession p : Profession.values()) {
+            if(p.screenName != null && p.screenName.equals(s)) {
                 return p;
             }
         }
